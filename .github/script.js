8e35377cd760218e13ce18f448417fcfaf7bf8a2
Linden <50274080+liulinden@users.noreply.github.com>
2024-03-04 13:21:58 -0800

    level and stuff

--- .github/script.js
+++ .github/script.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById('cvs')
+const txt = document.getElementById('text')
 document.addEventListener("keydown", keyDown)
 document.addEventListener("keyup", keyUp)
 
@@ -51,9 +52,14 @@ function keyUp(event){
 
 function drawFrame(){
     player.draw()
-    for (let i=0;i<platforms.length;i++){
-        platforms[i].draw()
+    for (let i=0;i<objects.length;i++){
+        if (activated.length>0){
+            if(objects[i] ==activated[0]) objects[i].activate()
+            else objects[i].deactivate()
+        }
+        objects[i].draw()
     }
+    ctx.fillStyle="black";
     ctx.fillRect(0,Math.max(0,floorHeight),canvas.width,canvas.height)
 }
 
@@ -66,6 +72,8 @@ class Player{
     constructor(startx,starty,width=70,height=90){
         this.x=startx
         this.y=starty
+        this.checkX=startx
+        this.checkY=starty
         this.width=width
         this.height=height
         this.xSpeed=0
@@ -73,6 +81,7 @@ class Player{
         this.onGround=false;
     }
     tick(up=false,down=false,left=false,right=false){
+        activated=[]
         this.ySpeed+=2
         if (this.onGround) this.xSpeed*=0.6;
         else this.xSpeed*=0.9
@@ -90,21 +99,34 @@ class Player{
         let falling=this.ySpeed>0
         this.moveX();
         this.onGround= this.moveY() && falling;
+        if (this.y>1500) {
+            this.x=this.checkX
+            this.y=this.checkY
+            this.xSpeed=0
+            this.ySpeed=0
+            activated=[new Empty()]
+        }
+        if (this.xSpeed>13) direction="right";
+        else if (this.xSpeed<-13) direction="left";
+        if (direction=="right") scrollXGoal=this.x-700
+        else scrollXGoal=this.x-1400;
+        scrollXGoal=Math.max(0,scrollXGoal)
+        scrollX+=(scrollXGoal-scrollX)/20
     }
     moveY(){ //returns whether or not it collided
-        if (this.isColliding()){
+        if (this.isSolidColliding()){
             console.log("stuck in wall");
             return true;
         }
         this.y+=this.ySpeed;
-        if (this.isColliding()){
+        if (this.isSolidColliding()){
             if (this.ySpeed>0){
-                while (this.isColliding()) this.y-=5;
-                while (!this.isColliding()) this.y+=1;
+                while (this.isSolidColliding()) this.y-=5;
+                while (!this.isSolidColliding()) this.y+=1;
                 this.y-=1;
             } else{
-                while (this.isColliding()) this.y+=5;
-                while (!this.isColliding()) this.y-=1;
+                while (this.isSolidColliding()) this.y+=5;
+                while (!this.isSolidColliding()) this.y-=1;
                 this.y+=1;
             }
             this.ySpeed=0
@@ -113,19 +135,19 @@ class Player{
         return false;
     }
     moveX(){
-        if (this.isColliding()){
+        if (this.isSolidColliding()){
             console.log("stuck in wall");
             return true;
         }
         this.x+=this.xSpeed;
-        if (this.isColliding()){
+        if (this.isSolidColliding()){
             if (this.xSpeed>0){
-                while (this.isColliding()) this.x-=5;
-                while (!this.isColliding()) this.x+=1;
+                while (this.isSolidColliding()) this.x-=5;
+                while (!this.isSolidColliding()) this.x+=1;
                 this.x-=1;
             } else{
-                while (this.isColliding()) this.x+=5;
-                while (!this.isColliding()) this.x-=1;
+                while (this.isSolidColliding()) this.x+=5;
+                while (!this.isSolidColliding()) this.x-=1;
                 this.x+=1;
             }
             this.xSpeed=0;
@@ -133,22 +155,40 @@ class Player{
         }
         return false;
     }
-    isColliding(floor=true){ //
+
+    isSolidColliding(floor=true){ //platform specific
+        return this.collidesWithTypes(["platform","popup"],floor)
+    }
+
+    collidesWithTypes(types,floor=true){ //platform specific
         if (floor){
             if (this.y+this.height>floorHeight) return true;
         }
-        for (let i=0;i<platforms.length;i++){
-            if (this.collidesWith(platforms[i])) return true;
+        for (let i=0;i<objects.length;i++){
+            let inTypes=false
+            for (let j=0;j<types.length;j++){
+                if (objects[i].type==types[j]) {
+                    inTypes=true
+                    break
+                }
+            }
+            if (!inTypes) continue
+            if (this.collidesWith(objects[i])) {
+                if (objects[i].type=="popup") activated.push(objects[i])
+                return true;
+            }
         }
         return false;
     }
+
+
     collidesWith(object){
         if (this.x<object.x+object.width && this.x+this.width>object.x && this.y<object.y+object.height && this.y+this.height>object.y) return true;
         else return false;
     }
     draw(){
         ctx.fillStyle='black'
-        ctx.fillRect(Math.round(this.x-0.5),Math.round(this.y-0.5),Math.round(this.width+1),Math.round(this.height+1))
+        ctx.fillRect(Math.round(this.x-0.5-scrollX),Math.round(this.y-0.5),Math.round(this.width+1),Math.round(this.height+1))
         console.log()
     }
 }
@@ -159,44 +199,90 @@ class Platform{
         this.y=y;
         this.width=width;
         this.height=height;
-        this.type="platform"
+        this.type="platform";
+        this.color="black"
     }
     draw(){
-        ctx.fillRect(Math.round(this.x),Math.round(this.y),Math.round(this.width),Math.round(this.height))
+        ctx.fillStyle=this.color;
+        ctx.fillRect(Math.round(this.x-scrollX),Math.round(this.y),Math.round(this.width),Math.round(this.height))
     }
+    activate(){
+
+    }
+    deactivate(){}
     print(){
         console.log(this.x,this.y,this.width,this.height)
     }
 }
 
 class PopUp{
-    constructor(x,y){
+    constructor(text,link,x,y){
         this.x=x;
         this.y=y;
-        this.width=100;
-        this.height=100;
+        this.width=80;
+        this.height=80;
         this.type="popup"
+        this.color = "red"
+        this.yOffset = 0;
+        this.text=text;
+        this.link=link
     }
     draw(){
-        ctx.fillRect(Math.round(this.x),Math.round(this.y),Math.round(this.width),Math.round(this.height))
+        ctx.fillStyle=this.color;
+        ctx.fillRect(Math.round(this.x-scrollX),Math.round(this.y+this.yOffset),Math.round(this.width),Math.round(this.height))
+    }
+    activate(){
+        if (this.color=="red"){
+            this.color="lime"
+            txt.setAttribute("href",this.link)
+            txt.innerHTML=this.text;
+        }
+    }
+    deactivate(){
+        if (this.color=="lime"){
+            this.color="red"
+        }
     }
     print(){
         console.log(this.x,this.y,this.width,this.height)
     }
 }
 
+class Empty{
+    constructor(){
+
+    }
+    activate(){
+
+    }
+    deactivate(){}
+}
+
 
 var scrollX=0;
 var scrollY=0;
+var scrollXGoal=0;
+var direction = "right";
 var upArrow=false;
 var downArrow=false;
 var rightArrow=false;
 var leftArrow=false;
-var platforms=[
-    new Platform(650,540,150,160),
-    new Platform(300,400,100,50),
-    new Platform(600,200,100,50)
+var objects=[
+    new Platform(0,500,300,1000),
+    new Platform(800,400,300,50),
+    new Platform(1865,0,50,600),
+    new Platform(1800,800,400,50),
+    new Platform(1900,800,200,1000),
+    new Platform(2400,700,80,1000),
+    new Platform(1865,550,300,50),
+    new Platform(2720,650,250,1000),
+    new PopUp("Guessing Game / Input Output","https://liulinden.github.io/software-dev-for-web/guessing%20game",600,500),
+    new PopUp("Mood Board","https://liulinden.github.io/mood-board/",1400,700),
+    new PopUp("Pattern Machine","https://maxshurst.github.io/pattern-machine/",1700,450),
+    new PopUp("Flappy Bird","https://saas-computationalthinking.github.io/project-managing-genai-lauren-linden/",2000,300),
+    new PopUp("Tetris","https://liulinden.github.io/tetris/",2800,500)
 ]
-const floorHeight=700
-const player=new Player(0,0);
+const floorHeight=2000
+const player=new Player(100,100);
+var activated = []
 gameloop();
